e434ba6b7f9b1a409db4df8e5918ada6f6e10e1d
vaishnaviavalur74 <vaishnaviavalur74@gmail.com>
2026-02-28 17:29:24 +0530

    Add files via upload
    day 1 Assignments

---
+++ day1/Firefox.java
@@ -0,0 +1,23 @@
+package org.testing.week1;
+
+public class Firefox 
+{
+public static void main(String args[])
+{
+
+float Browserversion =100.2f;
+System.out.println(Browserversion);
+
+String BrowserName = "firefox";
+System.out.println(BrowserName);
+
+boolean IsVisible = true;
+System.out.println(IsVisible);
+
+int ReleaseYear =1998;
+System.out.println(ReleaseYear);
+ 
+char BrowserLogo = 'f';
+System.out.println(BrowserLogo);
+}
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package org.testing.week1.day1;
+
+public class FirstProgram 
+{
+    public static void main(String args[])
+    {
+    System.out.println("hello vaishnavi");
+
+    }
+   
+
+}
